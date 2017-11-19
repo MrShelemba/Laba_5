fc797e9355a0b30b1b62426fd110fbc59e5d2743
Unknown <mrshelemba@gmail.com>
2017-11-19 02:08:34 +0200

    Виконав завдання 1.5.

--- lab05_1_main.m
+++ lab05_1_main.m
@@ -51,15 +51,39 @@ fprintf('Дисперсія сигналу = %4.3g\n', var(x))
 %=== Завдання #1.3 ===
 % Обчислення незміщеної оцінки АКФ змодельованого процесу  
 maxlag = fix(0.2*length(x));
-[r, lags] = xcorr(x, maxlag, 'unbiased');
+[v, lags] = xcorr(x, x, maxlag, 'unbiased');
 figure(2)
-plot(lags, r); title('Незміщена АКФ змодельованого процесу');
-xlim([-5 5]);
+plot(lags, v); title('Незміщена АКФ змодельованого процесу');
 
 %=== Завдання #1.4 ===
 % Обчислення зміщеної оцінки АКФ змодельованого процесу  
 maxlag2 = fix(0.8*length(x));
-[r2, lags2] = xcorr(x, maxlag2, 'biased');
+[v2, lags2] = xcorr(x, x, maxlag2, 'biased');
 figure(3)
-plot(lags2, r2); title('Зміщена АКФ змодельованого процесу');
-xlim([-5 5]);
+plot(lags2, v2); title('Зміщена АКФ змодельованого процесу');
+
+%=== Завдання #1.5 ===
+% Обчислення оцінки АКФ змодельованого процесу при збільшенні тривалості процесу 
+%t=100
+t3 = 0:100; % вектор часу
+v3 = randn(size(t3))*sqrt(D);
+s3 = A*cos(2*pi*f0*t3);
+x3 = s3+v3;
+
+maxlag3 = fix(0.3*length(x3));
+[r3, lags3] = xcorr(x3, x3, maxlag3, 'unbiased');
+figure(4)
+plot(lags3, r3);
+xlim([-30 30]);
+
+%t=1000
+t4 = 0:1000; % вектор часу
+v4 = randn(size(t4))*sqrt(D);
+s4 = A*cos(2*pi*f0*t4);
+x4 = s4+v4;
+
+maxlag4 = fix(0.3*length(x4));
+[r4, lags4] = xcorr(x4, x4, maxlag4, 'unbiased');
+figure(5)
+plot(lags4, r4);
+xlim([-30 30]);
