cf152e256aec8716069526c65b3e1e4e19d0235f
Unknown <mrshelemba@gmail.com>
2017-11-22 22:52:30 +0200

    Виконав завдання 3.2.

--- lab05_3_main.m
+++ lab05_3_main.m
@@ -13,3 +13,13 @@ eeg_2 = load('eeg1-c3.dat');
 eeg2 = detrend(eeg_2);
 t = (0:length(eeg2)-1)/fs;
 subplot(2, 1, 2); plot(t, eeg2);
+
+%=== Завдання #3.2 ===
+% Виділення епохи сигналів ЕЕГ від t1 = 5,71 с до t2 = 6,78 с
+t1 = 5.71;
+t2 = 6.78;
+n1 = fix(t1*fs)+1;
+n2 = fix(t2*fs)+1;
+figure(15)
+subplot(2,1,1); plot(t(n1:n2), eeg1(n1:n2)), grid on;
+subplot(2,1,2); plot(t(n1:n2), eeg2(n1:n2)), grid on;
