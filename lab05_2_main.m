abf7e1ebacf125db769aeb7201ca515217f684d0
Unknown <mrshelemba@gmail.com>
2017-11-22 22:01:56 +0200

    Завдання 2.2.
    Також доробив завдання 1.2.

--- lab05_2_main.m
+++ lab05_2_main.m
@@ -2,8 +2,18 @@
 % Завантаження сигналу ЕЕГ файл (eeg1-p4.dat)
 fs = 100;
 eeg = load('eeg1-p4.dat');
-eeg1 = detrend(eeg);
-t = (0:length(eeg)-1)/fs;
+eeg1 = detrend(eeg1);
+t = (0:length(eeg1)-1)/fs;
 figure(6)
-plot(t, eeg);
+plot(t, eeg1);
+
+%=== Завдання #2.2 ===
+% Виділення епохи сигналу ЕЕГ від t1 = 4,7 с до t2 = 5,8 с
+t1 = 4.7;
+t2 = 5.8;
+n1 = (t1*fs)+1;
+n2 = (t2*fs)+1;
+t = (0:length(eeg1)-1)/fs;
+figure(7)
+plot(t(n1:n2), eeg1(n1:n2)), grid on;
 
