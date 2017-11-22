acc516fb42203dc0d91386739cf6742e8044c43b
Unknown <mrshelemba@gmail.com>
2017-11-22 23:37:56 +0200

    Виконав завдання 3.3-3.6.

--- lab05_3_6_main.m
+++ lab05_3_6_main.m
@@ -1,36 +1,37 @@
-
-%=== Завдання #3.1 ===
-% Завантаження сигналу ЕЕГ файл (eeg1-o2.dat)
+%=== Завдання #3.6 ===
+% Завантаження сигналу ЕЕГ файл (eeg1-f3.dat)
 fs = 100;
-eeg_1 = load('eeg1-o2.dat');
+eeg_1 = load('eeg1-f3.dat');
 eeg1 = detrend(eeg_1);
 t_1 = (0:length(eeg1)-1)/fs;
-figure(14)
+figure(22)
 subplot(2, 1, 1); plot(t_1, eeg1);
 
-% Завантаження сигналу ЕЕГ файл (eeg1-c3.dat)
-eeg_2 = load('eeg1-c3.dat');
+% Завантаження сигналу ЕЕГ файл (eeg1-f4.dat)
+eeg_2 = load('eeg1-f4.dat');
 eeg2 = detrend(eeg_2);
 t_2 = (0:length(eeg2)-1)/fs;
 subplot(2, 1, 2); plot(t_2, eeg2);
 
-%=== Завдання #3.2 ===
-% Виділення епохи сигналів ЕЕГ від t1 = 5,71 с до t2 = 6,78 с
-t1 = 5.71;
-t2 = 6.78;
+% Виділення епохи сигналів ЕЕГ від t1 = 4,13 с до t2 = 4,96 с
+t1 = 4.13;
+t2 = 4.96;
+t3 = 1.44;
+t4 = 2.26;
 n1 = fix(t1*fs)+1;
 n2 = fix(t2*fs)+1;
-figure(15)
+n3 = fix(t3*fs)+1;
+n4 = fix(t4*fs)+1;
+figure(23)
 subplot(2,1,1); plot(t_1(n1:n2), eeg1(n1:n2)), grid on;
-subplot(2,1,2); plot(t_2(n1:n2), eeg2(n1:n2)), grid on;
+subplot(2,1,2); plot(t_2(n3:n4), eeg2(n3:n4)), grid on;
 
-%=== Завдання #3.3 ===
 % Обчислення ВКФ сигналів ЕЕГ
-vkf = xcorr(eeg1, eeg2, maxlag, 'unbiased');
-figure(16)
+maxlag = 250;
+vkf = xcorr(eeg1, eeg2, maxlag);
+figure(24)
 plot(vkf);
 
-%=== Завдання #3.4 ===
 % Обчислення взаємної спектральної щільності сигналів ЕЕГ
 Sx = abs(fft(vkf)/length(vkf));
 Sy = Sx';
@@ -38,5 +39,5 @@ Syf = [Sy(1),2.*Sy(2:end-1),Sy(end)];
 Syn = fftshift(Syf);
 N = length(Sy);
 t3 = (0:N-1)/N*fs;
-figure(17)
+figure(25)
 plot(t3, Syn), grid on;
