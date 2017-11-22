1e99bce0ad73d88fed6b007cffa2da28170f10b5
Unknown <mrshelemba@gmail.com>
2017-11-22 22:29:07 +0200

    Завдання 2.5.

--- lab05_2_main.m
+++ lab05_2_main.m
@@ -11,8 +11,8 @@ plot(t, eeg1);
 % Виділення епохи сигналу ЕЕГ від t1 = 4,7 с до t2 = 5,8 с
 t1 = 4.7;
 t2 = 5.8;
-n1 = (t1*fs)+1;
-n2 = (t2*fs)+1;
+n1 = fix(t1*fs)+1;
+n2 = fix(t2*fs)+1;
 figure(7)
 plot(t(n1:n2), eeg1(n1:n2)), grid on;
 
@@ -22,3 +22,16 @@ maxlag = fix(0.8*length(eeg1));
 akf = xcorr(eeg1, eeg1, maxlag, 'unbiased');
 figure(8)
 plot(akf);
+
+%=== Завдання #2.5 ===
+% Обчислення спектральної щільності сигналу
+Sx = abs(fft(eeg1)/length(eeg1));
+Sy = Sx';
+Syf = [Sy(1),2.*Sy(2:end-1),Sy(end)];
+Syn = fftshift(Syf);
+N = length(Sy);
+t3 = (0:N-1)/N*fs;
+figure(9)
+plot(t3, Syn), grid on;
+
+
